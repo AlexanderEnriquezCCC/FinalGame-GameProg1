70dc357111efbe12b537c298f7997d61ffd5e2ac
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-11-30 23:39:08 -0600

    adds Proof of Concept folder w/ interface for weapons

---
+++ ProofOfConcept/ProofOfConcept/Models/Weapons/IWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOfConcept.Models.Weapons
+{
+    interface IWeapon
+    {
+        int Damage { get; set; }
+        string Name { get; set; }
+        string Type { get; set; }
+        string Verb { get; set; }
+
+    }
+}
