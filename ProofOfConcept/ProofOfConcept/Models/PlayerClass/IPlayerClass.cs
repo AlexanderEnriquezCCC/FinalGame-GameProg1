a6b1fea67ab85647f39224342a7aab983e944918
AlexanderEnriquezCCC <44684320+AlexanderEnriquezCCC@users.noreply.github.com>
2020-12-01 15:24:27 -0600

    adds PlayerClass, adds vertical slice (nothing in it)

---
+++ ProofOfConcept/ProofOfConcept/Models/PlayerClass/IPlayerClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProofOfConcept.Models.PlayerClass
+{
+    interface IPlayerClass
+    {
+        int HP { get; set; }
+        int Defence { get; set; }
+        int Damage { get; set; }
+        int DamageModifier { get; set; } //might put this in weapons
+
+
+    }
+}
